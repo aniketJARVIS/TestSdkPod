8b0581787607d6e4d804f076cb33bd3a3dd8237f
Aniket <aniket@ost.com>
2019-03-13 16:03:43 +0530

    Init proj

---
+++ PodSdkTest/PodTestSwift.swift
@@ -0,0 +1,15 @@
+//
+//  PodTestSwift.swift
+//  PodSdkTest
+//
+//  Created by aniket ayachit on 13/03/19.
+//  Copyright © 2019 aniket ayachit. All rights reserved.
+//
+
+import Foundation
+
+public class PodTestSwift {
+    public class func podTestFunc() -> String {
+        return "Returning from podTestFunc"
+    }
+}
